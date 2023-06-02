8d0c7da7a506e113ec5e7e45b0c5cb78010ebed8
simona2606 <simonaettari26@gmail.com>
2023-06-02 18:07:56 +0200

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Simona Ettari on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
